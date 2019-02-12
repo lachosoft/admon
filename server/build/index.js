366144519878226214aa8e3063f4239b2c761beb
dhhg <mr.horacio@gmail.com>
2019-02-11 22:39:35 -0500

    cnfiguracion de puerto para el servidor

--- server/build/index.js
+++ server/build/index.js
@@ -1,2 +1,27 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var Server = /** @class */ (function () {
+    function Server() {
+        this.app = express_1.default(); //almacenamos express en app
+        this.config();
+        this.routes();
+    }
+    Server.prototype.config = function () {
+        this.app.set('port', process.env.PORT || 3000);
+    };
+    Server.prototype.routes = function () {
+    };
+    Server.prototype.start = function () {
+        this.app.listen(this.app.get('port'));
+        {
+            console.log('Server on port', this.app.get('port'));
+        }
+    };
+    return Server;
+}());
+var server = new Server();
+server.start();
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,30 @@
-import express from "express";
+import express, {Application} from "express";
+import { config } from "bluebird";
+
+class Server{
+
+    public app: Application; //defincioon de variable para express
+
+    constructor(){
+        this.app = express(); //almacenamos express en app
+        this.config();
+        this.routes();
+    }
+
+    config(): void{
+        this.app.set('port', process.env.PORT || 3000);
+    }
+
+    routes():void{
+
+    }
+
+    start(): void{
+        this.app.listen(this.app.get('port')){
+            console.log('Server on port', this.app.get('port'));
+        }
+    }
+}
+
+const server =new Server();
+server.start();
