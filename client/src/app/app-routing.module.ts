717a9983436af64af8afa30cb11576cb94de9f5f
dhhg <mr.horacio@gmail.com>
2019-02-13 22:31:16 -0500

    se creo la ruta Users

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {UserListComponent} from './components/user-list/user-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/users',
+    pathMatch: 'full'
+  },
+  {
+    path: 'users',
+    component: UserListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
