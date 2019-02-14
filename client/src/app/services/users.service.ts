d3cad43e24c4fd127242a2dc7a12412e83d386dd
dhhg <mr.horacio@gmail.com>
2019-02-13 23:39:00 -0500

    se corrigio el metodo updateUser

--- client/src/app/services/users.service.ts
+++ client/src/app/services/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 
 import {User} from '../modules/User';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +21,7 @@ export class UsersService {
     return this.http.get('${this.API_URI}/users/${id}');
   }
 
-  updateUser(id: string, user: User){
+  updateUser(id: string, user: User): Observable<User> {
     return this.http.put('${this.API_URI}/users/${id}',user);
   }
 
