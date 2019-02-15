44cb4d973fdf8a283db20163ac2dc007f2f1b1e3
dhhg <mr.horacio@gmail.com>
2019-02-14 21:12:59 -0500

    se agrego al modulo HttpClientModule en app.module.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {HttpClientModule} from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavegationComponent } from './components/navegation/navegation.component';
@@ -18,7 +20,8 @@ import {UsersService} from './services/users.service'
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [
     UsersService
