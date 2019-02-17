d74862d532e5a363d267e4bb7874e4b744875b1a
dhhg <mr.horacio@gmail.com>
2019-02-16 22:21:18 -0500

    se creo el objeto usr y se vinculo con el componente user-form.component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -21,7 +23,8 @@ import {UsersService} from './services/users.service'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [
     UsersService
--- client/src/app/components/user-form/user-form.component.ts
+++ client/src/app/components/user-form/user-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { User } from '../../modules/User';
+
+import {UsersService} from '../../services/users.service'
 
 @Component({
   selector: 'app-user-form',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserFormComponent implements OnInit {
 
-  constructor() { }
+  @HostBinding('class') clasese = "row";
+
+  usr: User = {
+    idUsr : 0,
+    nick : ``,
+	  nombres : ``,
+	  appaterno : ``,
+	  apmaterno : ``,
+	  email : ``,
+    deleted : 0,
+    deletedon : new Date(),
+    deletedby : 0,
+    createdon : new Date(),
+	  createdby : 0
+  };
+
+  constructor(private userService : UsersService) {
+    
+  }
 
   ngOnInit() {
+  }
+
+  saveUser()
+  {
+    console.log(this.usr);
   }
 
 }
