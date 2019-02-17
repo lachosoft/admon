a9230b947764cad91c0961a183baa43073753ee8
dhhg <mr.horacio@gmail.com>
2019-02-16 19:29:03 -0500

    se corrigio el diseño de user-list.component y se agrego en app-routing el modulo add

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import {UserListComponent} from './components/user-list/user-list.component';
+import {UserFormComponent} from './components/user-form/user-form.component'
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'users',
     component: UserListComponent
+  },
+  {
+    path: 'users/add',
+    component: UserFormComponent
   }
 ];
 
